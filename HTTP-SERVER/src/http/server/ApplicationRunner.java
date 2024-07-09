6cab0caa0c5c3148658c651e9cc1c5f5fec907b1
petar-Atanasov <PA673@live.mdx.ac.uk>
2024-07-09 11:43:11 +0100

    HTTP Server completed

--- HTTP-SERVER/src/http/server/ApplicationRunner.java
+++ HTTP-SERVER/src/http/server/ApplicationRunner.java
@@ -1,14 +1,31 @@
 package http.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) throws Exception {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
-        while(true){
-            // spin forever
+        while(true){ // spin forever
+           final Socket client = server.accept();
+//           1. Read HTTP request from the client socket
+//           2. Prepare an HTTP response
+//           3. Send HTTP response to the client 
+//           4. Close the socket 
+            
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            
+            String line = reader.readLine();
+            while(!line.isEmpty()){
+                System.out.println(line);
+                line = reader.readLine();
+            }
         }
     }
     
